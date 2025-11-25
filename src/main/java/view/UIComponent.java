9cd45737ea245ae8e67c54d7fe083941e1c105b1
Kassandra Estrada <kassandraestrada@Kassandras-MacBook-Air-3.local>
2025-11-25 00:57:51 -0500

    Add AdminPage

---
+++ src/main/java/view/UIComponent.java
@@ -0,0 +1,16 @@
+package view;
+
+import service.Backend;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class UIComponent extends JPanel {
+    protected final Backend backend;
+    protected final JFrame frame;
+
+    public UIComponent(JFrame frame, Backend backend) {
+        this.backend = backend;
+        this.frame = frame;
+    }
+}
