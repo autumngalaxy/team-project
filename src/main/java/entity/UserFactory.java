435aebeb9c5d9cfc60e0847ec280f4f97404f933
Kassandra Estrada <kassandraestrada@Kassandras-MacBook-Air-3.local>
2025-11-26 00:25:10 -0500

    Resolve merge conflicts

--- src/main/java/entity/UserFactory.java
+++ src/main/java/entity/UserFactory.java
@@ -1,7 +1,7 @@
 package entity;
 
-//public class UserFactory {
-//    public User create(String name, String password) {
-//        return new User(name, password);
-//    }
-//}
+public class UserFactory {
+    public User create(String name, String password) {
+        return new User(0, name, "", User.idType.PHOTO_CARD, 0, "", "", password);
+    }
+}
