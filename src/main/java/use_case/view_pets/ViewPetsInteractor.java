85b8712a725ae92b2539284e07600264ed49f98d
Angelica Pozzuoli <angelicapozzuoli@gmail.com>
2025-11-30 21:31:17 -0500

    Implemented ViewPetsInteractor

--- src/main/java/use_case/view_pets/ViewPetsInteractor.java
+++ src/main/java/use_case/view_pets/ViewPetsInteractor.java
@@ -1,4 +1,28 @@
 package use_case.view_pets;
+import data_access.PetDataAccessInterface;
+import java.util.List;
+import entity.Pet;
 
-public class ViewPetsInteractor {
+public class ViewPetsInteractor implements ViewPetsInputBoundary {
+    private final PetDataAccessInterface petDataAccess;
+    private final ViewPetsOutputBoundary presenter;
+
+    public ViewPetsInteractor(PetDataAccessInterface petDataAccess,
+                              ViewPetsOutputBoundary presenter) {
+        this.petDataAccess = petDataAccess;
+        this.presenter = presenter;
+    }
+    @Override
+    public void execute(ViewPetsInputData inputData) {
+        try {
+            List<Pet> pets = petDataAccess.getPets();
+            ViewPetsOutputData outputData =
+                    new ViewPetsOutputData(pets, null);
+            presenter.present(outputData);
+        } catch (Exception e) {
+            ViewPetsOutputData outputData =
+                    new ViewPetsOutputData(List.of(), "Could not load pets.");
+            presenter.present(outputData);
+        }
+    }
 }
