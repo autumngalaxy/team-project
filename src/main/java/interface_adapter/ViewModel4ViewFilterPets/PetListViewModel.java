22d5961312fcd266b55db292ecaa35873285a9b4
Angelica Pozzuoli <angelicapozzuoli@gmail.com>
2025-11-30 21:42:18 -0500

    Implemented ViewPetsPresenter

--- src/main/java/interface_adapter/ViewModel4ViewFilterPets/PetListViewModel.java
+++ src/main/java/interface_adapter/ViewModel4ViewFilterPets/PetListViewModel.java
@@ -1,4 +1,27 @@
+
 package interface_adapter.ViewModel4ViewFilterPets;
 
+import entity.Pet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PetListViewModel {
+
+    private List<Pet> pets = new ArrayList<>();
+    private String errorMessage;
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
+
